c01aa12d29602cf0fd1c150a217281f553d25864
Ann <annie.wang50@yahoo.com>
2021-06-15 23:11:51 -0400

    store router & react packages

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const morgan = require("morgan")
+const storeRouter = require("./routes/store")
 
 const app = express()
 
 app.use(morgan("tiny"))
 app.use(express.json())
+app.use("/store", storeRouter)
 
 app.get("/", async(req, res, next) => {
   res.status(200).json()
