fbf229d4625b37ccdef38094d49d761e91d82ad2
Ann <annie.wang50@yahoo.com>
2021-06-17 22:39:42 -0400

    store items style

--- ui/src/components/Home/Home.js
+++ ui/src/components/Home/Home.js
@@ -9,7 +9,6 @@ export default function Home( {products} ) {
         {/* <div className="college-name">College of Codepath</div> */}
         <div className="intro">
           <h1>Welcome!</h1>
-          <h1>Find Your Merch!</h1>
           <p>
             We have all kinds of goodies. Click on any of the items to start filling up your shopping cart. 
             Checkout whenever you're ready.
@@ -20,12 +19,12 @@ export default function Home( {products} ) {
       <div id="about">
         <h1>About</h1>
         <div className="about">
-          <div class="text">
+          <div className="text">
             <p>The codepath student store offers great products at great prices from a great team and for a great cause.</p>
             <p>We've searched far and wide for items that perk the interests of even the most eccentric students and decided to offer them all here in one place.</p>
             <p>All proceeds go towards bringing high quality CS education to college students around the country.</p>
           </div>
-          <div class="media">
+          <div className="media">
             <img src={codepath} alt="codepath large"/>
           </div>
         </div>
@@ -39,8 +38,8 @@ export default function Home( {products} ) {
       <div id="contact">
         <h1>Contact</h1>
         <div className="contact">
-          <div class="summary">
-            <ul class="info">
+          <div className="summary">
+            <ul className="info">
               <li><span>Email:</span> code@path.org</li>
               <li><span>Phone:</span> 1-800-CODEPATH</li>
               <li><span>Address:</span> 123 Fake Street, San Francisco, CA</li>
