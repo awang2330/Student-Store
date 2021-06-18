96ec5cfc6454936d4ecdbdbc012f77ceceef8d41
Ann <annie.wang50@yahoo.com>
2021-06-18 13:09:44 -0400

    add to cart buttons

--- ui/src/components/Sidebar/Sidebar.js
+++ ui/src/components/Sidebar/Sidebar.js
@@ -16,12 +16,14 @@ export default function Sidebar() {
       <div className={isOpen ? "open" : "close"}>
         <div className="sidebar-title">Shopping Cart</div>
         <table>
-          <tr>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>Unit Price</th>
-            <th>Cost</th>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Unit Price</th>
+              <th>Cost</th>
+            </tr>
+          </tbody>
         </table>
       </div>
       <button className="sidebar-btn" onClick={handleOnClick}>{isOpen ? <>&#171;</> : <>&#187;</> }</button>
--- ui/src/components/StoreProducts/StoreProducts.js
+++ ui/src/components/StoreProducts/StoreProducts.js
@@ -2,22 +2,34 @@ import './StoreProducts.css'
 import { Link } from 'react-router-dom'
 
 export default function StoreProducts( {products} ) {
-  
+  const handleIncrement = () => {
+
+  }
+
+  const handleDecrement = () => {
+
+  }
+
   return (
     <div className="StoreProducts">
       {products.map((item) => (
-        <Link to={`/store/${item.id}`} key={item.id}>
           <div className="product-info product-hover" key={item.id}>
-            <div className="product-image">
-              <img src={item.image} alt={item.name}/>
-            </div>
+            <Link to={`/store/${item.id}`} key={item.id}>
+              <div className="product-image">
+                <img src={item.image} alt={item.name}/>
+              </div>
+            </Link>
             <div className="product-details">
+              <div className="addtocart-btns">
+                <button onClick={handleIncrement}>–</button>
+                <button onClick={handleDecrement}>+</button>
+              </div>
               <div className="product-category">{item.category}</div>
               <div className="product-name">{item.name}</div>
               <div className="product-price">${item.price = item.price = parseFloat(item.price).toFixed(2)}</div>
             </div>
           </div>
-        </Link>
+        
       ))}
     </div>
   )
